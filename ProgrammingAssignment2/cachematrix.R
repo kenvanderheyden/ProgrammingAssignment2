dbfce1a28e749ec4a5de68ab41028ac26d8a184d
Ken Vanderheyden <ken.vanderheyden@archimiddle.com>
2015-08-18 15:45:57 +0200

    commit in the correct directory

--- ProgrammingAssignment2/cachematrix.R
+++ ProgrammingAssignment2/cachematrix.R
@@ -37,10 +37,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x' ( using solve(matrix) function to return the inverse )
     m <- x$getmatrix()
+    
     if (!is.null(m)) {
         message("getting cached data")
         return(m)
     }
+    
     data <- x$get()
     m <- solve(data, ...)
     x$setmatrix(m)
